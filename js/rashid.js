26f4a980034c306f4c4a4565551700fec7588e70
Diego Gaspar <88562658+gaspartv@users.noreply.github.com>
2022-06-04 23:44:42 -0300

    Clean Code

--- js/rashid.js
+++ js/rashid.js
@@ -1,4 +1,4 @@
-let localRashid = new Array (
+const localRashid = new Array (
     "Carlin", // 0
     "Svargrond", // 1
     "Liberty Bay", // 2
@@ -8,7 +8,7 @@ let localRashid = new Array (
     "Edron" // 6
     );
 
-let imgRashid = new Array(
+const imgRashid = new Array(
     "./img/carlinmap.png", // 0
     "./img/svarmap.png", // 1
     "./img/lbmap.png", // 2
@@ -19,73 +19,75 @@ let imgRashid = new Array(
     );
 
 let dia = new Date();
-let h = dia.getHours();
+let hora = dia.getHours();
 let diaSemana = (localRashid[dia.getDay()]);
-let servesave = 5;
-let classRashid = "criatura-dia-img-1";
+const classRashid = "imgMapRashid";
+const servesave = 5;
+const docRashidDay = document.getElementById('rashid-day')
+const docRashidImg = document.getElementById("rashid-img")
 
-function time() {
-    if (diaSemana == localRashid[0]) {
-        if (h >= servesave) {
-            document.getElementById('rashid-day').innerHTML = localRashid[0];
-            document.getElementById("rashid-img").innerHTML = "<img class='" + classRashid + "' src='" + imgRashid[0] + "'>";
+switch (diaSemana) {
+    case localRashid[0]:
+        if (hora >= servesave) {
+            docRashidDay.innerHTML = localRashid[0];
+            docRashidImg.innerHTML = "<img class='" + classRashid + "' src='" + imgRashid[0] + "'>";
         } else {
-            document.getElementById('rashid-day').innerHTML = localRashid[6];
-            document.getElementById("rashid-img").innerHTML = "<img class='" + classRashid + "' src='" + imgRashid[6] + "'>";
+            docRashidDay.innerHTML = localRashid[6];
+            docRashidImg.innerHTML = "<img class='" + classRashid + "' src='" + imgRashid[6] + "'>";
         }
-    }
-    if (diaSemana == localRashid[1]) {
-        if (h >= servesave) {
-            document.getElementById('rashid-day').innerHTML = localRashid[1];
-            document.getElementById("rashid-img").innerHTML = "<img class='" + classRashid + "' src='" + imgRashid[1] + "'>";
+    break;
+    case localRashid[1]:
+        if (hora >= servesave) {
+            docRashidDay.innerHTML = localRashid[1];
+            docRashidImg.innerHTML = "<img class='" + classRashid + "' src='" + imgRashid[1] + "'>";
         } else {
-            document.getElementById('rashid-day').innerHTML = localRashid[0];
-            document.getElementById("rashid-img").innerHTML = "<img class='" + classRashid + "' src='" + imgRashid[0] + "'>";
+            docRashidDay.innerHTML = localRashid[0];
+            docRashidImg.innerHTML = "<img class='" + classRashid + "' src='" + imgRashid[0] + "'>";
         }
-    }
-    if (diaSemana == localRashid[2]) {
-        if (h >= servesave) {
-            document.getElementById('rashid-day').innerHTML = localRashid[2];
-            document.getElementById("rashid-img").innerHTML = "<img class='" + classRashid + "' src='" + imgRashid[2] + "'>";
+    break;
+    case localRashid[2]:
+        if (hora >= servesave) {
+            docRashidDay.innerHTML = localRashid[2];
+            docRashidImg.innerHTML = "<img class='" + classRashid + "' src='" + imgRashid[2] + "'>";
         } else {
-            document.getElementById('rashid-day').innerHTML = localRashid[1];
-            document.getElementById("rashid-img").innerHTML = "<img class='" + classRashid + "' src='" + imgRashid[1] + "'>";
+            docRashidDay.innerHTML = localRashid[1];
+            docRashidImg.innerHTML = "<img class='" + classRashid + "' src='" + imgRashid[1] + "'>";
         }
-    }
-    if (diaSemana == localRashid[3]) {
-        if (h >= servesave) {
-            document.getElementById('rashid-day').innerHTML = localRashid[3];
-            document.getElementById("rashid-img").innerHTML = "<img class='" + classRashid + "' src='" + imgRashid[3] + "'>";
+    break;
+    case localRashid[3]:
+        if (hora >= servesave) {
+            docRashidDay.innerHTML = localRashid[3];
+            docRashidImg.innerHTML = "<img class='" + classRashid + "' src='" + imgRashid[3] + "'>";
         } else {
-            document.getElementById('rashid-day').innerHTML = localRashid[2];
-            document.getElementById("rashid-img").innerHTML = "<img class='" + classRashid + "' src='" + imgRashid[2] + "'>";
+            docRashidDay.innerHTML = localRashid[2];
+            docRashidImg.innerHTML = "<img class='" + classRashid + "' src='" + imgRashid[2] + "'>";
         }
-    }
-    if (diaSemana == localRashid[4]) {
-        if (h >= servesave) {
-            document.getElementById('rashid-day').innerHTML = localRashid[4];
-            document.getElementById("rashid-img").innerHTML = "<img class='" + classRashid + "' src='" + imgRashid[4] + "'>";
+    break;
+    case localRashid[4]:
+        if (hora >= servesave) {
+            docRashidDay.innerHTML = localRashid[4];
+            docRashidImg.innerHTML = "<img class='" + classRashid + "' src='" + imgRashid[4] + "'>";
         } else {
-            document.getElementById('rashid-day').innerHTML = localRashid[3];
-            document.getElementById("rashid-img").innerHTML = "<img class='" + classRashid + "' src='" + imgRashid[3] + "'>";
+            docRashidDay.innerHTML = localRashid[3];
+            docRashidImg.innerHTML = "<img class='" + classRashid + "' src='" + imgRashid[3] + "'>";
         }
-    }
-    if (diaSemana == localRashid[5]) {
-        if (h >= servesave) {
-            document.getElementById('rashid-day').innerHTML = localRashid[5];
-            document.getElementById("rashid-img").innerHTML = "<img class='" + classRashid + "' src='" + imgRashid[5] + "'>";
+    break;
+    case localRashid[5]:
+        if (hora >= servesave) {
+            docRashidDay.innerHTML = localRashid[5];
+            docRashidImg.innerHTML = "<img class='" + classRashid + "' src='" + imgRashid[5] + "'>";
         } else {
-            document.getElementById('rashid-day').innerHTML = localRashid[4];
-            document.getElementById("rashid-img").innerHTML = "<img class='" + classRashid + "' src='" + imgRashid[4] + "'>";
+            docRashidDay.innerHTML = localRashid[4];
+            docRashidImg.innerHTML = "<img class='" + classRashid + "' src='" + imgRashid[4] + "'>";
         }
-    }
-    if (diaSemana == localRashid[6]) {
-        if (h >= servesave) {
-            document.getElementById('rashid-day').innerHTML = localRashid[6];
-            document.getElementById("rashid-img").innerHTML = "<img class='" + classRashid + "' src='" + imgRashid[6] + "'>";
+    break;
+    case localRashid[6]:
+        if (hora >= servesave) {
+            docRashidDay.innerHTML = localRashid[6];
+            docRashidImg.innerHTML = "<img class='" + classRashid + "' src='" + imgRashid[6] + "'>";
         } else {
-            document.getElementById('rashid-day').innerHTML = localRashid[5];
-            document.getElementById("rashid-img").innerHTML = "<img class='" + classRashid + "' src='" + imgRashid[5] + "'>";
+            docRashidDay.innerHTML = localRashid[5];
+            docRashidImg.innerHTML = "<img class='" + classRashid + "' src='" + imgRashid[5] + "'>";
         }
-    }
+    break;
 }
